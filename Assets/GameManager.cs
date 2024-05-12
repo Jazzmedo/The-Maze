a3f961fae6d1be7aa7c215bab3df01871bc568c7
abdullahwaleedd <140311915+abdullahwaleedd@users.noreply.github.com>
2024-05-12 03:43:38 +0200

    1.2

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static int difficultE=10; // Variable for difficulty level
-    public static int coindiff=15; // Variable for difficulty name (optional)
+    public static int coindiff=15; 
 
     void Awake()
     {
-        DontDestroyOnLoad(gameObject); // Persist across scenes
+        DontDestroyOnLoad(gameObject); 
     }
     
 }
